c936a8c59ee374f57b523bd52391c9b2f0f53ce8
mustafa <iammustafaterzi@gmail.com>
2026-02-10 19:02:17 +0300

    Chapter-02/OperatorsAndConditionals.cs

---
+++ ifelsedersi.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+
+public class ifelsedersi : MonoBehaviour
+{
+
+    /* karþýlaþtýrma operatörleri
+                
+         < küçükse
+         > büyükse
+         >= büyükse ve eþitse
+         <= küçükse ve eþitse
+         == eþitse
+         != eþit deðise
+
+    *
+    * */
+
+
+          
+            
+           
+    int sayi1 = 5;
+    int sayi2 = 10;
+    int sayi3 = 3;
+    int sayi4 = 5;
+    string ad1 = "ahmet";
+    string ad2 = "ahmet";
+    bool kapiacikmi = false;
+
+    /* mantýksal operatörler
+           not iþaretei => ! (ifade true ise false, false ise true yapar)
+           and iþareti =>  &&
+           or iþareti =>  ||
+
+           
+           * */
+
+    void Start()
+    {
+        /*
+        if(sayi1 < sayi2)
+            Debug.Log("koþul saðlandý");
+        else if (sayi1 < sayi2)
+            Debug.Log("koþul saðlandý");
+        else
+            Debug.Log("koþul saðlanmadý");
+
+        */
+
+        /*
+
+        if (!kapiacikmi)
+                {
+                    Debug.Log("deðiþken not ile deðiþtirildi");
+                }
+                else
+                {
+                    Debug.Log("deðiþken false");
+
+                }
+
+        if (sayi1 < sayi2 || sayi1 < sayi3 || sayi1 < sayi4)
+        {
+            Debug.Log("1. Koþul");
+
+        }
+        else if (sayi1 < sayi4)
+        {
+            Debug.Log("3. Koþul");
+
+        }
+        else if (ad1 == ad2)
+        {
+            Debug.Log("4. Koþul");
+
+        }
+        else
+        {
+            Debug.Log("Hayýr Deðil");
+        }
+        */
+        
+      /*  if (kosul)
+        {
+              // bu kosul saglanirsa burasi calisir
+
+        }
+        else if (kosul2)
+        {
+
+        }
+        else
+        {
+            // kosul saglanmazsa burasi calisir
+        }*/
+
+    }
+
+
+}
